a5dc1c79e1457b5546700720f22fdc78c0881ca2
Kevin Wilburn <wilburnkevin200@gmail.com>
2021-07-22 19:16:01 -0400

    pulling both GET requests

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-<<<<<<< HEAD
-
-app.use('/api/collections', collections)
-=======
 app.use('/api/collections', collections);
->>>>>>> ab978355dee1a8668e99202460ea8e8828454f22
 
 app.listen(5000, function () {
     console.log("Server started. Listening on port 5000.");
--- routes/collections.js
+++ routes/collections.js
@@ -5,9 +5,28 @@ const { Post, validatePost } = require("../models/post");
 
 //GET Return all user data  /collections/api/user Kevin
 
+router.get("/allUsers", async (req, res) =>{
+    try{
+        const user = await User.find();
+        return res.send(user);
+
+    }catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+    
+})
+
 
 //GET Returns all posts 	 /collections/api/posts Kevin
 
+router.get("/allPosts" , async (req,res)=>{ 
+    try{
+        const post = await Post.find();
+        return res.send(post);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
+})
 
 //POST Create a user 	/collections/api/register
 router.post("/register", async (req, res) => {
@@ -46,6 +65,8 @@ router.post("/register", async (req, res) => {
 
 
 //DELETE Deletes a currently logged in user's posts /collections/api/delete/post Giancarlo
+
+
 
 
 module.exports = router;
